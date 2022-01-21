2c7cce187a3cede377c0b4d12673735cb236929e
Camila Cristina <criscamila09@gmail.com>
2022-01-21 13:35:36 -0300

    save theme local storage

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-function darkTheme() {
-  document.querySelector('#switchContainer').classList.toggle('light')
-  document.querySelector('.switchButton').classList.toggle('light')
+const themeValue = localStorage.getItem('selectedTheme')
+const defaultTheme = 'light'
+
+document.querySelector('#switchContainer').classList.value = themeValue
+
+const saveTheme = function () {
+  const theme = document.querySelector('#switchContainer').classList.value
+  localStorage.setItem('selectedTheme', theme)
+  return
 }
 
-darkTheme()
+function darkTheme() {
+  document.querySelector('#switchContainer').classList.toggle(defaultTheme)
+  saveTheme()
+  return
+}
